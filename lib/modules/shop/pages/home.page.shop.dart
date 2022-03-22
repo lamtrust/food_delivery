9554dbe323873baf706419d43510debe91af4a0e
iamngoni <imngonii@gmail.com>
2022-03-22 21:39:10 +0200

    gitlab remote

--- lib/modules/shop/pages/home.page.shop.dart
+++ lib/modules/shop/pages/home.page.shop.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/extensions/context.extension.dart';
+import 'package:relative_scale/relative_scale.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,6 +12,63 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return RelativeBuilder(builder: (context, height, width, sy, sx) {
+      return Container(
+        height: context.height,
+        width: context.width,
+        padding: EdgeInsets.only(
+          top: sy(10),
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Deliver to",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: sy(13),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.location_on_outlined,
+                            size: sy(12),
+                          ),
+                          SizedBox(
+                            width: sx(5),
+                          ),
+                          Text(
+                            "136 Blakeway Drive, Belvedere",
+                            style: TextStyle(
+                              fontSize: sy(9),
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: FadeInImage.assetNetwork(
+                    placeholder: 'assets/images/loading.gif',
+                    image: 'https://picsum.photos/250?image=9',
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
