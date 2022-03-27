9eac58bc920f5250b16a8aef9bf8c9a27f99f7d0
iamngoni <imngonii@gmail.com>
2022-03-27 22:50:21 +0200

    back from product page & fav button

--- lib/modules/shop/pages/product.page.dart
+++ lib/modules/shop/pages/product.page.dart
@@ -30,101 +30,146 @@ class _ProductPageState extends State<ProductPage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Hero(
-                tag: widget.product.id,
-                child: Container(
-                  height: sy(200),
-                  width: context.width,
-                  color: const Color(0xFFE9EEFF),
-                  alignment: Alignment.center,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      SizedBox(
-                        height: sy(10),
-                      ),
-                      Expanded(
-                        child: Container(
-                          padding: EdgeInsets.symmetric(
-                            horizontal: sx(20),
+              Stack(
+                children: [
+                  Hero(
+                    tag: widget.product.id,
+                    child: Container(
+                      height: sy(200),
+                      width: context.width,
+                      color: const Color(0xFFE9EEFF),
+                      alignment: Alignment.center,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          SizedBox(
+                            height: sy(10),
                           ),
-                          width: context.width,
-                          alignment: Alignment.center,
-                          child: FadeInImage.assetNetwork(
-                            placeholder: "assets/images/loading.gif",
-                            image: widget.product.images.first,
-                            fit: BoxFit.cover,
-                            placeholderFit: BoxFit.scaleDown,
-                            height: sy(99),
-                            width: context.width,
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: sx(20),
+                              ),
+                              width: context.width,
+                              alignment: Alignment.center,
+                              child: FadeInImage.assetNetwork(
+                                placeholder: "assets/images/loading.gif",
+                                image: widget.product.images.first,
+                                fit: BoxFit.cover,
+                                placeholderFit: BoxFit.scaleDown,
+                                height: sy(99),
+                                width: context.width,
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: sy(10),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: sx(20),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
+                          SizedBox(
+                            height: sy(10),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: sx(20),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                Text(
-                                  widget.product.name,
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: sy(10),
-                                  ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      widget.product.name,
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: sy(10),
+                                      ),
+                                    ),
+                                    Text(
+                                      widget.product.price > 1000
+                                          ? widget.product.price.money
+                                              .compactSymbolOnLeft
+                                          : widget
+                                              .product.price.money.symbolOnLeft,
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: sy(17),
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                                Text(
-                                  widget.product.price > 1000
-                                      ? widget.product.price.money
-                                          .compactSymbolOnLeft
-                                      : widget.product.price.money.symbolOnLeft,
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: sy(17),
-                                  ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    Text(
+                                      "⭐" * widget.product.rating.ceil(),
+                                      style: TextStyle(
+                                        fontSize: sy(11),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: sy(5),
+                                    ),
+                                    Text(
+                                      "(${widget.product.rating})",
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.normal,
+                                        fontSize: sy(10),
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ],
                             ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                Text(
-                                  "⭐" * widget.product.rating.ceil(),
-                                  style: TextStyle(
-                                    fontSize: sy(11),
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: sy(5),
-                                ),
-                                Text(
-                                  "(${widget.product.rating})",
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.normal,
-                                    fontSize: sy(10),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
+                          ),
+                          SizedBox(
+                            height: sy(10),
+                          ),
+                        ],
                       ),
-                      SizedBox(
-                        height: sy(10),
-                      ),
-                    ],
+                    ),
                   ),
-                ),
+                  Positioned(
+                    top: sy(10),
+                    left: sx(20),
+                    child: InkWell(
+                      onTap: () => context.goBack(),
+                      child: Container(
+                        height: 35,
+                        width: 35,
+                        decoration: BoxDecoration(
+                          color: AppColors.darkBlue,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Icon(
+                          Icons.arrow_back_rounded,
+                          color: Colors.white,
+                          size: sy(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: sy(10),
+                    right: sx(20),
+                    child: InkWell(
+                      onTap: () => context.goBack(),
+                      child: Container(
+                        height: 35,
+                        width: 35,
+                        decoration: BoxDecoration(
+                          color: AppColors.darkBlue,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Icon(
+                          Icons.favorite_outline,
+                          color: Colors.white,
+                          size: sy(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
               SizedBox(
                 height: sy(10),
